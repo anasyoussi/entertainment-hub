252e3818e9c338b021f09674a34cec05d0a223b7
anasyoussi <18307320+anasyoussi@users.noreply.github.com>
2021-11-03 18:53:02 +0100

    SignleContent.js & Trending.js

---
+++ src/Components/SingleContent/SingleContent.js
@@ -0,0 +1,9 @@
+function SingleContent({id, poster, title, date, media_type, vote_average}) {
+    return (
+        <div>
+            <div> {title} </div>
+        </div>
+    )
+}
+
+export default SingleContent
--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,33 +1,36 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import SingleContent from "../../Components/SingleContent/SingleContent";
 
-const Trending = () => {
-    const [content, setContent] = useState([]);
-
-    const fetchTrending = async () => { 
-        const { data } = await axios.get(
-            `https://api.themoviedb.org/3/trending/all/day?api_key=015bed65873f4bd49b5c7ce0fe0131b3`
-        ); 
-        console.log(data);
-        setContent(data.result) 
-    }
-
+const Trending = () => { 
+    const [content, setContent] = useState([]) 
+    const fetchTrending = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`);
+        setContent(data.results);
+    }; 
     useEffect(() => {
-        fetchTrending() ;
-    }, [])
-
+        fetchTrending()
+    }, []) 
     return (
         <div>
-            <span className='pageTitle'>Trending</span>
-            <div className='trending'>
-                {
-                    content && content.map((c) => {
-                        console.log(c)
-                    })
-                }
-            </div>
+          <span className="pageTitle">Trending</span>
+          <div className="trending"> 
+              {
+                content && content.map((c) => 
+                    <SingleContent 
+                        key={c.id} 
+                        id={c.id} 
+                        poster={c.poster_path} 
+                        title={c.title || c.name} 
+                        date={c.first_air_date || c.release_date} 
+                        media_type={c.media_type}
+                        vote_average={c.vote_average}
+                    />)
+              }
+          </div>
         </div>
-    )
-}
+    );
+
+};
 
 export default Trending;
